7193ce43325850beb96c32eaa99916b80c950639
flyship <79181438+yuxuan-fan@users.noreply.github.com>
2024-12-19 16:06:13 +0800

    优化结构

--- train.py
+++ train.py
@@ -3,8 +3,6 @@ warnings.filterwarnings('ignore')
 from ultralytics import RTDETR
 
 if __name__ == '__main__':
-    # model = RTDETR(r'E:\githubFYX\RTDETR-main\ultralytics\cfg\models\rt-detr\rtdetr-Faster.yaml')
-    # model.load('D:\\workspace\\github\\RTDETR-main\\weights\\rtdetr-r18.pt') # loading pretrain weights
     model = RTDETR(r'E:\githubFYX\RTDETR-main\ultralytics\cfg\models\rt-detr\rtdetr-r18.yaml')
     model.train(data='dataset/data.yaml',
                 cache=False,
@@ -15,7 +13,6 @@ if __name__ == '__main__':
                 
                 device='0',
                 # device='cpu', #本地调试使用cpu
-
                 # resume='', # last.pt path
                 project='runs/train',
                 name='deeppcb',
