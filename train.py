37dcb4934c7d311490d149c7f9cf387ed1eae70a
范范范 <fanyx@e.gzhu.edu.cn>
2025-09-22 19:50:05 +0800

    Update train.py

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ warnings.filterwarnings('ignore')
 from ultralytics import RTDETR
 
 if __name__ == '__main__':
-    model = RTDETR(r'ultralytics/cfg/models/rt-detr/rtdetr-r18.yamll')
+    model = RTDETR(r'ultralytics/cfg/models/rt-detr/rtdetr-r18.yaml')
     model.train(data='dataset/data.yaml',
                 cache=False,
                 imgsz=640,
